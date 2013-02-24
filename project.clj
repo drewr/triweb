4bb9db41d08916793b7b4b7ad30765990b6e0de0
Drew Raines <aaraines@gmail.com>
2013-02-23 22:50:39 -0600

    enlive-1.1.1.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0-RC16"]
                  [compojure "1.1.5"]
-                 [enlive "1.1.0" :exclude [org.clojure/clojure]]
+                 [enlive "1.1.1" :exclude [org.clojure/clojure]]
                  [me.raynes/cegdown "0.1.0" :exclude [org.clojure/clojure]]
                  [ring "1.1.8"]
                  [sonian/carica "1.0.2"]]
