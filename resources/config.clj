8214ba8237593b7a10df9fe705fdc2fab61bdf80
Drew Raines <aaraines@gmail.com>
2013-02-24 16:19:12 -0600

    Add trinity doc dir.

--- resources/config.clj
+++ resources/config.clj
@@ -1,4 +1,6 @@
 {:template
  {:roots ["tmpl"
           (format "%s/Dropbox/Trinity-WWW/www.tcnash.org"
-                  (System/getProperty "user.home"))]}}
+                  (System/getProperty "user.home"))
+          "/home/trinity/Dropbox/Trinity-WWW/www.tcnash.org"
+          ]}}
