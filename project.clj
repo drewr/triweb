8c2fe98af23bf2f2ab25290c6aa9afa47b86c945
Andrew Raines <andrew@raines.me>
2014-11-20 09:30:54 -0600

    Update ring deps

--- project.clj
+++ project.clj
@@ -12,9 +12,8 @@
                  [me.raynes/cegdown "0.1.0" :exclude [org.clojure/clojure]]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/data.xml "0.0.7"]
-                 [ring "1.1.8"]
+                 [ring "1.3.1"]
                  [sonian/carica "1.0.2"]
                  [org.clojure/core.memoize "0.5.3"]]
-  :profiles {:dev {:dependencies [[ring/ring-devel "1.1.8"]]}}
-  :plugins [[lein-ring "0.8.3"]]
+  :plugins [[lein-ring "0.8.13"]]
   :ring {:handler triweb.handler/app})
