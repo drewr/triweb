585d12e661fadb67dc18dae1053298cfcd5dd95f
Drew Raines <drew@raines.me>
2015-03-09 16:29:28 -0700

    encode.hs WIP

---
+++ bin/encode.hs
@@ -0,0 +1,53 @@
+#!/usr/bin/env runhaskell
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+import Turtle
+import Data.Yaml (decodeEither)
+import Data.Aeson.TH ( deriveJSON
+                     , defaultOptions
+                     , fieldLabelModifier
+                     , constructorTagModifier
+                     )
+import Data.Text (Text, unpack)
+import Data.Char (toLower)
+import qualified Data.ByteString.Char8 as B
+import Options.Applicative
+
+defaultBucket = "media.trinitynashville.org"
+
+main :: IO ()
+main = do
+  o <- execParser $ info (helper <*> opts) (header "Trinity Audio Encoder")
+  j <- getContents
+  putStrLn $ case (decodeEither (B.pack j) :: Either String PodcastInput) of
+    (Left err) -> "\n*** problem reading information file:\n\n" ++ err ++ "\n"
+    (Right s) -> (unpack . pcComment $ s) ++ "\n" ++ (bucket o)
+
+data Opts = Opts { mp3SrcPath :: String
+                 , bucket :: String
+                 }
+
+opts :: Parser Opts
+opts = Opts <$> argument str (metavar "MP3")
+       <*> strOption
+           ( long "bucket"
+           <> metavar "BUCKET"
+           <> help "What bucket?"
+           <> value defaultBucket )
+
+
+data PodcastInput = PodcastInput
+  { pcFilePrefix :: Text
+  , pcAuthor     :: Text
+  , pcYear       :: Int
+  , pcComposer   :: Text
+  , pcAlbum      :: Text
+  , pcGenre      :: Text
+  , pcComment    :: Text
+  } deriving (Show)
+
+$(deriveJSON
+  defaultOptions { fieldLabelModifier = (map toLower) . (drop 2)
+                 } ''PodcastInput)
