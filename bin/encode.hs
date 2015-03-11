9ed9e59e0d2e12c3521e0c5ac8e71021f9f0b402
Drew Raines <drew@raines.me>
2015-03-10 19:28:00 -0700

    Add title construction

--- bin/encode.hs
+++ bin/encode.hs
@@ -10,12 +10,24 @@ import Data.Aeson.TH ( deriveJSON
                      , fieldLabelModifier
                      , constructorTagModifier
                      )
-import Data.Text (Text, unpack)
+import Data.Text (Text, unpack, pack)
 import Data.Char (toLower)
 import qualified Data.ByteString.Char8 as B
-import Options.Applicative
+import Options.Applicative as Options
+import Options.Applicative ( execParser
+                           , info
+                           , helper
+                           , header
+                           , argument
+                           , str
+                           , metavar
+                           , strOption
+                           , long
+                           , value)
+import System.FilePath.Posix (takeBaseName)
 
 defaultBucket = "media.trinitynashville.org"
+defautlAuthor = "Trinity Church"
 
 main :: IO ()
 main = do
@@ -23,20 +35,31 @@ main = do
   j <- getContents
   putStrLn $ case (decodeEither (B.pack j) :: Either String PodcastInput) of
     (Left err) -> "\n*** problem reading information file:\n\n" ++ err ++ "\n"
-    (Right s) -> (unpack . pcComment $ s) ++ "\n" ++ (bucket o)
+    (Right s) -> (unpack . pcComment $ s) ++ "\n" ++ (bucket o) ++ "\n" ++ (encodeTitle s o)
+
+class Dated a where
+  datedString :: a -> String
+
+class Encodable a where
+  encodeTitle :: Dated b => a -> b -> String
+
+instance Dated Opts where
+  datedString o = takeBaseName . mp3SrcPath $ o
+
+instance Encodable PodcastInput where
+  encodeTitle pi o = (datedString o) ++ "-" ++ (unpack . pcFilePrefix $ pi)
 
 data Opts = Opts { mp3SrcPath :: String
                  , bucket :: String
                  }
 
-opts :: Parser Opts
+opts :: Options.Parser Opts
 opts = Opts <$> argument str (metavar "MP3")
        <*> strOption
            ( long "bucket"
            <> metavar "BUCKET"
            <> help "What bucket?"
            <> value defaultBucket )
-
 
 data PodcastInput = PodcastInput
   { pcFilePrefix :: Text
