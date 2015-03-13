a4da9317604a9f06add89b586e99cd7b80ba8f2b
Drew Raines <drew@raines.me>
2015-03-13 13:38:26 -0700

    Working lame encoder

--- bin/encode.hs
+++ bin/encode.hs
@@ -3,14 +3,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-import Turtle
+import Turtle hiding (option)
+import GHC.IO.Exception
 import Data.Yaml (decodeEither)
 import Data.Aeson.TH ( deriveJSON
                      , defaultOptions
                      , fieldLabelModifier
                      , constructorTagModifier
                      )
-import Data.Text (Text, unpack, pack)
+import Data.Text (Text, unpack, pack, intercalate)
 import Data.Char (toLower)
 import qualified Data.ByteString.Char8 as B
 import Options.Applicative as Options
@@ -21,37 +22,73 @@ import Options.Applicative ( execParser
                            , argument
                            , str
                            , metavar
+                           , option
+                           , auto
                            , strOption
                            , long
                            , value)
 import System.FilePath.Posix (takeBaseName)
+import System.Directory
 
 defaultBucket = "media.trinitynashville.org"
-defautlAuthor = "Trinity Church"
+defaultAuthor = "Trinity Church"
+defaultImage  = "../resources/static/img/podcast4.png"
+defaultScale  = 3
 
 main :: IO ()
 main = do
   o <- execParser $ info (helper <*> opts) (header "Trinity Audio Encoder")
   j <- getContents
-  putStrLn $ case (decodeEither (B.pack j) :: Either String PodcastInput) of
-    (Left err) -> "\n*** problem reading information file:\n\n" ++ err ++ "\n"
-    (Right s) -> (unpack . pcComment $ s) ++ "\n" ++ (bucket o) ++ "\n" ++ (encodeTitle s o)
+  case (decodeEither (B.pack j) :: Either String PodcastInput) of
+    (Left err) -> putStrLn $ "\n*** problem reading information file:\n\n" ++ err ++ "\n"
+    (Right pi) -> do
+      encode "voice" (pack . mp3SrcPath $ o) (encodeDest pi o)
+      encode "256" (pack . mp3SrcPath $ o) (encodeDest pi o)
+      return ()
+
+encode :: Text -> Text -> Text -> IO ()
+encode preset src dest = do
+  lame "scale!" preset src dest
+
+lame :: Text -> Text -> Text -> Text -> IO ()
+lame scale preset src dest =
+  loggingProc "lame"
+       [ "-S"
+       , "--scale", scale
+       , "--preset", preset
+       , src
+       , dest
+       ]
+
+loggingProc :: Text -> [Text] -> IO ()
+loggingProc cmd args = do
+  echo $ format ("running: "%s%" "%s) cmd (intercalate " " args)
+  ret <- proc cmd args empty
+  case ret of
+     ExitSuccess -> return ()
+     ExitFailure err -> die (cmd <> " has failed: " <> (repr err))
 
 class Dated a where
-  datedString :: a -> String
+  datedString :: a -> Text
 
 class Encodable a where
-  encodeTitle :: Dated b => a -> b -> String
+  encodeTitle    :: Dated b => a -> b -> Text
+  encodeDestHifi :: Dated b => a -> b -> Text
+  encodeDest     :: Dated b => a -> b -> Text
 
 instance Dated Opts where
-  datedString o = takeBaseName . mp3SrcPath $ o
+  datedString o = pack . takeBaseName . mp3SrcPath $ o
 
 instance Encodable PodcastInput where
-  encodeTitle pi o = (datedString o) ++ "-" ++ (unpack . pcFilePrefix $ pi)
+  encodeTitle pi o    = (datedString o) <> "-" <> (pcFilePrefix pi)
+  encodeDestHifi pi o = (encodeTitle pi o) <> "-HIFI.mp3"
+  encodeDest pi o     = (encodeTitle pi o) <> ".mp3"
 
 data Opts = Opts { mp3SrcPath :: String
-                 , bucket :: String
-                 }
+                 , bucket     :: String
+                 , imagePath  :: String
+                 , audioScale :: Int
+                 } deriving (Show)
 
 opts :: Options.Parser Opts
 opts = Opts <$> argument str (metavar "MP3")
@@ -60,6 +97,16 @@ opts = Opts <$> argument str (metavar "MP3")
            <> metavar "BUCKET"
            <> help "What bucket?"
            <> value defaultBucket )
+       <*> strOption
+           ( long "imagePath"
+           <> metavar "IMAGE"
+           <> help "What image to attach to the podcast"
+           <> value defaultImage )
+       <*> option auto
+           ( long "audioScale"
+           <> metavar "IMAGE"
+           <> help "What image to attach to the podcast"
+           <> value defaultScale )
 
 data PodcastInput = PodcastInput
   { pcFilePrefix :: Text
