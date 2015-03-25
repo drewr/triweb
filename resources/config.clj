dabc0ef79da3e91c4b0c5e2450407f55d18a9921
Drew Raines <drew@raines.me>
2015-03-25 16:36:36 -0500

    Fix config order, name

--- resources/config.clj
+++ resources/config.clj
@@ -1,6 +1,6 @@
 {:template
- {:roots ["tmpl"
-          (format "%s/Dropbox/Trinity-WWW/www.tcnash.org"
+ {:roots [(format "%s/Dropbox/Trinity-WWW/www.trinitynashville.org"
                   (System/getProperty "user.home"))
-          "/home/trinity/Dropbox/Trinity-WWW/www.tcnash.org"
+          "/home/trinity/Dropbox/Trinity-WWW/www.trinitynashville.org"
+          "tmpl"
           ]}}
