422bb1ec647ac7530bccfaf684b1cc74240f5c3a
Drew Raines <drew@raines.me>
2015-03-25 15:01:03 -0500

    user is mandatory

--- puppet/modules/web/manifests/init.pp
+++ puppet/modules/web/manifests/init.pp
@@ -20,6 +20,7 @@ class web (
   }
 
 ssh_authorized_key { $user:
+    user => $user,
     type => 'ssh-rsa',
     key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCqR1DVZtLibe37zsSz7H4iqbHllTFUYfN0reDd/gCRHvrt7+RBpOxrUthSac+OxY+n/ORll62VGqcVmHOeAY6X4YQVzrSMdwDuljw7U0yg21kZGhVfiyfsWKaPWDC0d/B+9kpwB/67Q3KNACIBWgV1jiI6za5AiEn+1LCQJbSxS2u4E8cC3XBcF6Ifmxiw40GM44AFpTMwqYn3zsK2eM02m7nn49UpbLa417+YkqJZ94La1f3Ag0CpeZaA1qFIM6eEq/olxgOcby7tJHJ+/AQ9oMnn7WCHo51+63gCR6bj7z7TgGGwRiSUnNBbMfQ6E2QT1OiXASFuVILWOs5dbhGj',
   }  
