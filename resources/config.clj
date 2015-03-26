38708317b47593917926c8c1a87467b91259dc1b
Drew Raines <drew@raines.me>
2015-03-25 22:17:21 -0500

    No, template precedence was right

--- resources/config.clj
+++ resources/config.clj
@@ -1,6 +1,6 @@
 {:template
- {:roots [(format "%s/Dropbox/Trinity-WWW/www.trinitynashville.org"
+ {:roots ["tmpl"
+          (format "%s/Dropbox/Trinity-WWW/www.trinitynashville.org"
                   (System/getProperty "user.home"))
           "/home/trinity/Dropbox/Trinity-WWW/www.trinitynashville.org"
-          "tmpl"
           ]}}
