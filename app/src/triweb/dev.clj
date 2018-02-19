69e3590ee82e5650636eb849dd67a6c9a2492ceb
Drew Raines <drew@raines.me>
2018-02-19 13:58:24 -0600

    enlive-reload doesn't work, needs 'load-file, tabling it for now

--- app/src/triweb/dev.clj
+++ app/src/triweb/dev.clj
@@ -1,12 +1,12 @@
 (ns triweb.dev
   (:require [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.reload :refer [wrap-reload]]
-            [com.akolov.enlive-reload :refer [wrap-enlive-reload]]
             [triweb.http :refer [app]]
             ))
 
 (defonce server
   (run-jetty (-> #'app
+                 ;; Doesn't account for enlive
                  wrap-reload
-                 wrap-enlive-reload)
+                 )
              {:port 3000 :join? false}))
